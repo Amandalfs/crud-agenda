613ec34da83e7bb6c6b77f0f274ff0b605ba2842
Amandalfs <caduc57@gmail.com>
2023-04-14 17:59:19 -0300

    feat
    criado a rota login

--- db/tableUsers.js
+++ db/tableUsers.js
@@ -1,21 +1,23 @@
 const knex = require('knex')
 const knexConfig = require('../knexfile');
-const e = require('express');
 
 const db = knex(knexConfig.development)
 
-db.schema.createTable("users", function (tabela) {
-    tabela.increments("id_user").primary();
-   tabela.text("name").notNull();
-   tabela.text("email").notNull();
-   tabela.text("senha").notNull();
- }).then(function () {
-   console.log('Tabela "users" criada com sucesso!');
- }).catch(function (error) {
-   console.error('Erro ao criar tabela "users": ', error);
- })
- 
-/* 
-db.schema.dropTable('users', function(){
+function up(){
+  db.schema.createTable("users", function (tabela) {
+      tabela.increments("id_user").primary();
+      tabela.text("name").notNull();
+      tabela.text("email").notNull();
+      tabela.text("senha").notNull();
+   }).then(function () {
+     console.log('Tabela "users" criada com sucesso!');
+   }).catch(function (error) {
+     console.error('Erro ao criar tabela "users": ', error);
+   })
+}
 
-}) */
+function down(){
+  db.schema.dropTable('users', function(){
+
+  })
+}
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const exp = require('constants');
 const express = require('express');
 const port = 8020;
 const cors = require('cors');
-const createUser = require('./routes/users/createUser')
+const createUser = require('./routes/users/createUser');
+const login = require('./routes/users/loginUser');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 //routes 
 createUser(app);
+login(app);
 
 app.get('/', (req,res)=>{
     res.send("Bem vindo ao crud da agenda")
