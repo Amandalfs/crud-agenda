2084dd28b33c35cf1517180d994de2a589fdd77d
Amandalfs <caduc57@gmail.com>
2023-05-04 10:24:06 -0300

    tirado as importacoes que nao fazia sentido

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-const exp = require('constants');
 const express = require('express');
 const port = 8020;
 const cors = require('cors');
 
-const createUser = require('./routes/users/createUser');
-const login = require('./routes/users/loginUser');
-
-const adicionar = require('./routes/agenda/adicionar');
-const deleta = require('./routes/agenda/deleta');
-const contatos = require('./routes/agenda/contatos');
-const editar = require('./routes/agenda/editar');
 const routes = require('./routes');
 
 const app = express();
